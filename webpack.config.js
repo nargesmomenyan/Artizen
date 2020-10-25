526f7880e0247a16b67ac7b04578606a764aba30
Narges Momenyan <momenyan@gmail.com>
2020-10-25 22:58:05 +0330

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
  const path = require('path')
  const TerserPlugin = require('terser-webpack-plugin')
  const MiniCssExtractPlugin = require('mini-css-extract-plugin')
- const CleanWebpackPlugin = require('clean-webpack-plugin')
+ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
  const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports={
@@ -9,7 +9,7 @@ module.exports={
      output:{
          filename:'bundle.[contenthash].js',
          path: path.resolve( __dirname ,'./dist'),
-         publicPath: 'dist/'
+         publicPath: ''
          
      },
      mode:'none',
@@ -53,6 +53,12 @@ module.exports={
                     }
                 }
             ]
+        },
+        {
+            test:/\.hbs$/,
+            use:[
+                'handlebars-loader'
+            ]
         }
          ]
          
@@ -63,6 +69,10 @@ module.exports={
              filename:"styles.[contenthash].css"
          }),
          new CleanWebpackPlugin(),
-         new HtmlWebpackPlugin ()
+         new HtmlWebpackPlugin ({
+             title:"آرتیژن",
+             description:"طراحی سایت آرتیژن",
+             template:"src/pages/index.hbs"
+         })
      ]
 }
